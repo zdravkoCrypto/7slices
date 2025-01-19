29a54ea6fd1a84e0cbb7a2fc22ae51107f93343d
zdravkoCrypto <zdravko.jovanovic.tech@gmail.com>
2025-01-19 20:07:20 +0100

    somethin

--- app/components/catering.tsx
+++ app/components/catering.tsx
@@ -5,17 +5,17 @@ export default function Catering() {
   return (
     <div className="flex w-full">
       <div className="show-between-sizes flex flex-col w-full flex-grow">
-          <div className="flex w-full items-center justify-center p-6">
-              <div className="flex w-4/6 bg-black rounded-xl justify-between">
-                <div className="p-4 mt-12">
-                  <span className="font-semibold text-white">Catering 7 Slices</span>
+          <div className="show-between-sizes flex w-full items-center justify-center p-6">
+              <div className="show-between-sizes flex w-4/6 bg-black rounded-xl justify-between">
+                <div className="show-between-sizes p-4 mt-12">
+                  <span className="show-between-sizes font-semibold text-white">Catering 7 Slices</span>
                 </div>
                 <Image 
                   src="/catering.svg"  // Bildpfad (kann lokal oder extern sein)
                   alt=""
                   width={150}  // Bildbreite
                   height={150} // Bildhöhe
-                  className="show-between-sizes "
+                  className="show-between-sizes"
               />
              </div>
           </div>
--- app/components/rechtlichesbottom.tsx
+++ app/components/rechtlichesbottom.tsx
@@ -3,24 +3,24 @@ import Image from 'next/image'
 
 export default function Rechtlichesbottom() {
   return (
-    <div className="flex w-full p-6 bg-black">
+    <div className="show-between-sizes flex w-full p-6 bg-black">
         <Image 
           src="/7slices.jpg"  // Bildpfad (kann lokal oder extern sein)
           alt=""
           width={150}  // Bildbreite
           height={50} // Bildhöhe
-          className=""
+          className="show-between-sizes"
         />
-        <div className="flex flex-col w-full">
-          <span className="ml-4 mt-4 text-xs font-semibold text-white">Impressum:</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">E&K Gastro GmbH</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Mariahilferstraße 151</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">1150 Wien</span>
+        <div className="show-between-sizes flex flex-col w-full">
+          <span className="show-between-sizes ml-4 mt-4 text-xs font-semibold text-white">Impressum:</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">E&K Gastro GmbH</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Mariahilferstraße 151</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">1150 Wien</span>
 
-          <span className="ml-4 mt-4 text-xs font-semibold text-[#c7c5bb]">Tel.: +43 664 241 11 44</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Email: office@teslapizza.at</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">UID-Nr.: ATU7585847694</span>
-          <span className="ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Firmenbuchnummer: FN 48435423h</span>
+          <span className="show-between-sizes ml-4 mt-4 text-xs font-semibold text-[#c7c5bb]">Tel.: +43 664 241 11 44</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Email: office@teslapizza.at</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">UID-Nr.: ATU7585847694</span>
+          <span className="show-between-sizes ml-4 mt-2 text-xs font-semibold text-[#c7c5bb]">Firmenbuchnummer: FN 48435423h</span>
         </div>
     </div>
   )
